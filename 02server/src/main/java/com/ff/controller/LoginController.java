5d15ee9801c886224698f68b7941417647176038
yanghuan007 <870003013@qq.com>
2020-03-24 19:34:04 +0800

    权限、登录。注册

--- 02server/src/main/java/com/ff/controller/LoginController.java
+++ 02server/src/main/java/com/ff/controller/LoginController.java
@@ -20,33 +20,33 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("/noi")
 public class LoginController {
-//    @RequestMapping("/login")
-//    @ResponseBody
-//    public Msg Login(String loginName,String password, HttpServletRequest req, HttpServletResponse resp){
-//        Msg msg =new Msg();
-//        try {
-//            Subject subject = SecurityUtils.getSubject();
-//            password = MD5Utils.encrypt(loginName, password);
-//            UsernamePasswordToken token = new UsernamePasswordToken(loginName, password);
-//            subject.login(token);
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            msg.setMsg("登录失败");
-//            return msg;
-//        }
-//        msg.setMsg("登录成功");
-//        return msg;
-//    }
-//    @RequestMapping("/index")
-//    @ResponseBody
-//    public Msg Index(Model model){
-//        Msg msg =new Msg();
-//
-//        User user = (User) SecurityUtils.getSubject().getPrincipals();
-//        model.addAttribute("user",user);
-//        msg.setMsg("欢迎"+user.getName());
-//
-//        return msg;
-//    }
+    @RequestMapping("/login")
+    @ResponseBody
+    public Msg Login(String loginName,String password, HttpServletRequest req, HttpServletResponse resp){
+        Msg msg =new Msg();
+        try {
+            Subject subject = SecurityUtils.getSubject();
+            password = MD5Utils.encrypt(loginName, password);
+            UsernamePasswordToken token = new UsernamePasswordToken(loginName, password);
+            subject.login(token);
+        }catch (Exception e){
+            e.printStackTrace();
+            msg.setMsg("登录失败");
+            return msg;
+        }
+        msg.setMsg("登录成功");
+        return msg;
+    }
+    @RequestMapping("/index")
+    @ResponseBody
+    public Msg Index(Model model){
+        Msg msg =new Msg();
+
+        User user = (User) SecurityUtils.getSubject().getPrincipals();
+        model.addAttribute("user",user);
+        msg.setMsg("欢迎"+user.getName());
+
+        return msg;
+    }
 
 }

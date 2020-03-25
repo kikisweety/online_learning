7ab6e9762c02c4ad267852a86b4cbf9bc3acbf61
tianzhenqi <tianzhenqi@wakedata.com>
2020-03-26 00:47:31 +0800

    学生界面

--- 01client/02admin/src/views/student/components/Index/studentIndex.js
+++ 01client/02admin/src/views/student/components/Index/studentIndex.js
@@ -2,35 +2,32 @@ import React from "react";
 import "./studentIndex.css";
 import { Carousel } from 'antd';
 export default class MyIndex extends React.Component {
+    constructor() {
+        super();
+    }
     render() {
         return (
             <div className="indexBox">
                 <Carousel autoplay>
                     <div className="banner1">
                         <div className="bannerContain">
-                            <img src="/imgs/banner1.png"></img>
-                            <div className="bannerTitle">
-                                <h1>React+Vue精品课程</h1>
-                                <h3>前端教程入门到精通</h3>
-                            </div>
+                            <img src="/imgs/banner4.jpg"></img>
                         </div>
                     </div>
                     <div className="banner2">
                         <div className="bannerContain">
-                            <img src="/imgs/banner2.png"></img>
-                            <div className="bannerTitle">
-                                <h3>英语.数学.逻辑.写作</h3>
-                                <h1>真题课程包</h1>
-                                <h3>超高效的提分神器</h3>
-                            </div>
+                            <img src="/imgs/banner5.jpg"></img>
                         </div>
                     </div>
                     <div className="banner3">
                         <div className="bannerContain">
-                            <img src="/imgs/banner3.png"></img>
+                            <img src="/imgs/banner6.jpg"></img>
                         </div>
                     </div>
                 </Carousel>
+                <div className="rightList">
+
+                </div>
             </div>
         );
     }
